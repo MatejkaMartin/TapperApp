71bc35285800023d8ac91288f528686192873df7
Martin Prace <martinmatejka@martin-macbook-pro.local>
2016-03-06 11:39:18 +0100

    Initial Commit

---
+++ TapperApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapperApp
+//
+//  Created by Martin Matějka on 06.03.16.
+//  Copyright © 2016 Martin Matějka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
