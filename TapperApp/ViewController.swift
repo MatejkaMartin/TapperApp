38083ff12b2a88e83ca8e38b8851364a899475d3
Martin Prace <martinmatejka@martin-macbook-pro.local>
2016-03-22 22:28:31 +0100

    HOTOVO

--- TapperApp/ViewController.swift
+++ TapperApp/ViewController.swift
@@ -9,17 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+//OUtlets
+    
+    @IBOutlet weak var nadpisImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+//Variables
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    @IBAction func onKlikacJeZmacknut(sender: UIButton!)
+    {
+        currentTaps += 1
+        updateTapsLabel()
+        
+        if isGameOver(){
+            restartGame()
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onPlayBtnPressed(sender: UIButton!){
+       
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            nadpisImg.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            updateTapsLabel()
+            
+            
+        }
+        
     }
-
-
+    
+    func updateTapsLabel(){
+        tapsLbl.text = "\(currentTaps) Kliků"
+    }
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    
 }
 
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        nadpisImg.hidden = false
+        playBtn.hidden = false
+        howManyTapsTxt.hidden = false
+        
+        tapsLbl.hidden = true
+        tapBtn.hidden = true
+        
+    }
+}
